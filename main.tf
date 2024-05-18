46dc5b63073f7a7e1ca22e58168384a68d657342
MP <53264992+mykolapryvalov@users.noreply.github.com>
2024-05-18 21:08:08 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+module "github_repository" {
+  source                   = "github.com/mykolapryvalov/ft-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
+
+module "gke_cluster" {
+  source         = "github.com/mykolapryvalov/ft-gke-flux-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/mykolapryvalov/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+}
+
+module "tls_private_key" {
+  source = "github.com/mykolapryvalov/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
