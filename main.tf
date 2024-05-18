7fdc786941642e916bfc4be3af1cd3f9c6927d69
MP <53264992+mykolapryvalov@users.noreply.github.com>
2024-05-18 22:28:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "github_repository" {
-  source                   = "git::ssh://git@github.com/mykolapryvalov/ft-github-repository.git"
+  source                   = "github.com/mykolapryvalov/tf-github-repository"
   github_owner             = var.GITHUB_OWNER
   github_token             = var.GITHUB_TOKEN
   repository_name          = var.FLUX_GITHUB_REPO
@@ -8,20 +8,20 @@ module "github_repository" {
 }
 
 module "gke_cluster" {
-  source         = "git::ssh://git@github.com/mykolapryvalov/ft-gke-flux-cluster.git"
+  source         = "github.com/mykolapryvalov/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
 }
 
 module "flux_bootstrap" {
-  source            = "git::ssh://git@github.com/mykolapryvalov/tf-fluxcd-flux-bootstrap.git"
+  source            = "github.com/mykolapryvalov/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
   config_path       = module.gke_cluster.kubeconfig
 }
 
 module "tls_private_key" {
-  source = "git::ssh://git@github.com/mykolapryvalov/tf-hashicorp-tls-keys.git"
+  source = "github.com/mykolapryvalov/tf-hashicorp-tls-keys"
   algorithm = "RSA"
 }
